0d10ad138eb6fefc7de6e77f5ef64ecefc6febf7
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-16 10:56:54 -0300

    adicionando novas manipulacoes de lsita

---
+++ juntando-salas.js
@@ -0,0 +1,6 @@
+const salaJS = ["Evaldo", "Mario", "Denis"];
+const salaPython = ["Julia", "Leonardo", "Raquel"];
+
+const salasUnificadas = salaJS.concat(salaPython);
+
+console.log(salasUnificadas);
