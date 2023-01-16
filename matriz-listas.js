bd3955dc0166515a60344d430c97911cb80fdfdd
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-16 14:39:31 -0300

    novos testes de listas com listas

---
+++ matriz-listas.js
@@ -0,0 +1,7 @@
+const nomes = ["Ana", "Júlia", "Leonardo"];
+const idades = [30, 35, 28];
+const faculdade = [false, true, true];
+
+const funcionarios = [nomes, idades, faculdade];
+
+console.log(funcionarios[0][1])
