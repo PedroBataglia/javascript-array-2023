5d5f415b55e5b6d747078a44c6090e478db862f5
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-17 11:20:18 -0300

    estrturas de repeticao

---
+++ destruturing.js
@@ -0,0 +1,8 @@
+const numerosPares = [2, 4, 6]
+const numerosImpares = [1, 3, 5]
+
+const numeros = [...numerosPares, ...numerosImpares]
+
+const [num1, num2] = [1,2]
+
+console.log(numeros)
--- procurando-aluno.js
+++ procurando-aluno.js
@@ -5,12 +5,17 @@ const listaDeAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(aluno) {
     if (listaDeAlunosEMedias[0].includes(aluno)) {
-        console.log(`${aluno} está cadastrado`);
-        const indice = listaDeAlunosEMedias[0].indexOf(aluno);
-        console.log(indice);
+        // const alunos = listaDeAlunosEMedias[0];
+        // const medias = listaDeAlunosEMedias[1];
+
+        const [alunos, medias] = listaDeAlunosEMedias;
+
+        const indice = alunos.indexOf(aluno);
+
+        const mediaDoAluno = medias[indice];
+        console.log(`A média de ${aluno} é ${mediaDoAluno}.`)
     } else {
         console.log("Aluno não encontrado!");
     }
-}
-
-exibeNomeENota("João")
+} 
+exibeNomeENota("Juliana")
