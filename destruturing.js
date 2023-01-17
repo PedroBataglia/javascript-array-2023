74d081e15c4f16df9838cb5e2a80eca3f830da54
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-17 16:40:11 -0300

    novas funcoes

--- destruturing.js
+++ destruturing.js
@@ -3,6 +3,23 @@ const numerosImpares = [1, 3, 5]
 
 const numeros = [...numerosPares, ...numerosImpares]
 
-const [num1, num2] = [1,2]
+const [num1, num2, ...outrosValores] = [1, 2, 3, 4, 5]
 
-console.log(numeros)
+const pessoa = { 
+    nome: 'Pedro',
+    idade: '19'
+}
+
+const pessoaComTelefone = {...pessoa, telefone: 97304034}
+
+const { nome } = pessoa
+
+console.log(nome)
+
+
+function imprimeDados(dados) {
+    const {nome, idade} = dados
+    console.log(nome, idade)
+}
+
+imprimeDados(pessoa)
--- for.js
+++ for.js
@@ -1,5 +1,5 @@
 const numeros = [100, 200, 300, 400, 500, 600];
 
-for (i = 0; i < numeros.length; i++) {
-    console.log(numeros[i]);
+for (let indice = 0; indice < numeros.length; indice++) {
+    console.log(numeros[indice]);
 }
