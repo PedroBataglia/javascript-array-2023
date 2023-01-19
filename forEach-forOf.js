0e89e25a247cfcbe3afe34848c4bdc00cfa02e4e
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-19 10:02:25 -0300

    verificando o tempo de execussão

--- forEach-forOf.js
+++ forEach-forOf.js
@@ -1,21 +1,26 @@
 // for
+console.time()
 const elementos = [1, 2, 3, 4, 5];
 for (let i = 0, j = 0; i < elementos.length; i++, j++) {
     console.log(elementos[i] + j);
 }
+console.timeEnd()
 
 
 // forOF
+console.time()
 const itens = [1, 2, 3, 4, 5];
 let total = 0;
-
+ 
 for(let elemento of itens) {
     total += elemento;
 }
 
 console.log(total)
+console.timeEnd()
 
 // ForEach
+console.time()
 const lista = [1, 2, 3, 4, 5];
 
 let soma = 0;
@@ -23,3 +28,4 @@ let soma = 0;
 lista.forEach(numero => soma += numero);
 
 console.log(soma);
+console.timeEnd()
