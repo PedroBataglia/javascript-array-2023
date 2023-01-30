08da22cc845610d0f820e428f5e575ef35bc2973
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-30 09:00:52 -0300

    lista de listas (matriz)

---
+++ teste-array/lista-de-listas.js
@@ -0,0 +1,9 @@
+const champ = ["Aphelios", "Xayah", "Jhin","Varus"];
+const notas = [10, 9, 9, 8];
+const kda = ["13/2/15", "12/5/19", "7/0/9", "9/6/15"];
+
+const geralChamp = [champ, notas, kda];
+
+for (let i = 0; i < geralChamp.length; i++) {
+    console.log(geralChamp[i][0]);
+}
